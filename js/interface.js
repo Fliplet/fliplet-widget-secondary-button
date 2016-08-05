dbcc70e1d61c6c8f650e0cda99ff5479e5dfec48
Hugo Carneiro <hcarneiro@weboo.biz>
2016-08-05 16:34:33 +0100

    refactor of secondary button

--- js/interface.js
+++ js/interface.js
@@ -1,24 +1,25 @@
 var data = Fliplet.Widget.getData() || {};
 
+function getFormData() {
+  return {
+    label: $('#secondaryButtonLabel').val()
+  }
+}
+
+// Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  Fliplet.Widget.save(getFormData()).then(function () {
+    Fliplet.Widget.complete();
+  });
+})
+
 Fliplet.Widget.open('com.fliplet.link', {
   // If provided, the iframe will be appended here,
   // otherwise will be displayed as a full-size iframe overlay
   selector: '#action',
   // Also send the data I have locally, so that
   // the interface gets repopulated with the same stuff
-  data: data.action,
+  data: data.action
 }).then(function (result) {
   data.action = result.data;
-  $('form').submit();
-});
-
-$('form').submit(function (event) {
-  event.preventDefault();
-
-  data.label = $('#buttonLabel').val();
-
-  Fliplet.Widget.save(data).then(function () {
-    Fliplet.Widget.complete();
-    window.location.reload();
-  });
 });
