3c1b2ddd2b47bb92a22715fa63282534bc96c055
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-04 11:19:07 +0000

    tweak

--- js/build.js
+++ js/build.js
@@ -1,7 +1,7 @@
 $('[data-secondary-button-id]').click(function (event) {
   event.preventDefault();
 
-  var data = Fliplet.Widget.getData($(this).attr('data-secondary-button-id'));
+  var data = Fliplet.Widget.getData($(this).data('secondary-button-id'));
 
   Fliplet.Navigate.to(data.action);
 });
