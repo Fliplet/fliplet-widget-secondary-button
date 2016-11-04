0fa82b7e75a3aa6dd0c86179b588a46c0d0782dc
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-04 10:54:11 +0000

    Refactor click code

--- js/build.js
+++ js/build.js
@@ -1,7 +1,7 @@
-$('[data-button-id]').click(function (event) {
+$('[data-secondary-button-id]').click(function (event) {
   event.preventDefault();
 
-  var data = Fliplet.Widget.getData($(this).data('button-id'));
+  var data = Fliplet.Widget.getData($(this).attr('data-secondary-button-id'));
 
   Fliplet.Navigate.to(data.action);
 });
