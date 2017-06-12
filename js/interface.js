d58575f98aac9de1bc066ff162c06b47b34d916c
Hugo Carneiro <hcarneiro@weboo.biz>
2017-06-12 12:01:47 +0100

    Fix typo

--- js/interface.js
+++ js/interface.js
@@ -9,7 +9,7 @@ var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
   // the interface gets repopulated with the same stuff
   data: data.action,
   // Events fired from the provider
-  onEvent: function (event, data) {
+  onEvent: function(event, data) {
     if (event === 'interface-validate') {
       Fliplet.Widget.toggleSaveButton(data.isValid === true);
     }
@@ -18,18 +18,18 @@ var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
 });
 
 // 1. Fired from Fliplet Studio when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function () {
+Fliplet.Widget.onSaveRequest(function() {
   $('form').submit();
 });
 
 // 2. Fired when the user submits the form
-$('form').submit(function (event) {
+$('form').submit(function(event) {
   event.preventDefault();
   linkActionProvider.forwardSaveRequest();
 });
 
 // 3. Fired when the provider has finished
-linkActionProvider.then(function (result) {
+linkActionProvider.then(function(result) {
   data.action = result.data;
   save(true);
 });
@@ -37,7 +37,7 @@ linkActionProvider.then(function (result) {
 function save(notifyComplete) {
   data.label = $('#secondaryButtonLabel').val();
 
-  Fliplet.Widget.save(data).then(function () {
+  Fliplet.Widget.save(data).then(function() {
     if (notifyComplete) {
       Fliplet.Widget.complete();
       window.location.reload();
@@ -47,7 +47,7 @@ function save(notifyComplete) {
   });
 }
 
-$('#primaryButtonLabel').on('keyup change paste', $.debounce(function() {
+$('#secondaryButtonLabel').on('keyup change paste', $.debounce(function() {
   save();
 }, 500));
 
